15736396d15e6ac1b8cc79adf714e0c7ef2d2424
DilshodDeveloper-uz <dilshodsatimov990@mail.ru>
2023-01-05 14:39:03 +0500

    o'zgarish kiritildi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Salom hurmatli foydalanuvchi. Xush kelibsiz! ");
+System.Console.WriteLine("Iltimosh ismingizni kiriting: ");
+string UserName = Console.ReadLine();
+System.Console.WriteLine($" {UserName} tashakkur ");
