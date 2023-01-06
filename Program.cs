b9ba9b707fb41ef1ca721b28e723f120d0d61592
DilshodDeveloper-uz <dilshodsatimov990@mail.ru>
2023-01-06 17:20:31 +0500

    if operatorining uyga vazifasi

--- Program.cs
+++ Program.cs
@@ -1,4 +1,34 @@
-﻿System.Console.WriteLine("Salom hurmatli foydalanuvchi. Xush kelibsiz! ");
-System.Console.WriteLine("Iltimosh ismingizni kiriting: ");
-string UserName = Console.ReadLine();
-System.Console.WriteLine($" {UserName} tashakkur ");
+﻿System.Console.WriteLine("Ikkita butun son kiriting: ");
+System.Console.Write("birinchi son: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("ikkinchi son: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstnumber >0 && secondnumber>0)
+{
+    System.Console.WriteLine("Kiritilgan ikkala son ham musbat");
+}
+else
+if(firstnumber<0 && secondnumber<0)
+{
+    System.Console.WriteLine("Ikkala son ham manfiy");
+}
+else
+if(firstnumber==0 ||secondnumber==0)
+{
+    System.Console.WriteLine("ikkalasidan biri nolga teng ");
+}
+else
+if(firstnumber==0 && secondnumber==0)
+{
+    System.Console.WriteLine("ikkala son ham nolga teng");
+}
+else
+{
+    System.Console.WriteLine("kiritilgan sonlarning biri manfiy ikkinchisi musbat ");
+}
+Console.ReadKey();
+
+// System.Console.WriteLine("Iltimosh ismingizni kiriting: ");
+// string UserName = Console.ReadLine();                
+// System.Console.WriteLine($" {UserName} tashakkur ");
